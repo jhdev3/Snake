0ec908aefa8eaf5ce1eb8f45869e317ee3205eb2
Jacob Hedén <91886824+jhdev3@users.noreply.github.com>
2022-01-28 16:03:35 +0100

    FoodAndPlayer
    Branch created half done

---
+++ Snake/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{   
+    //NotMoving Enum Global Variable, Own class ?,. 
+    public enum Direction { Up, Down, Left, Right, NotMoving };
+
+    internal class Player : GameObject
+    {
+        public Direction playerMove = Direction.NotMoving; 
+        //Needs a Start postion
+        public Player (Posistion start)
+        {
+            position = start;
+            Appearance = 'X';
+            playerMove = Direction.NotMoving;
+        }
+
+        public override void Update()
+        {
+            switch (this.playerMove)
+            {
+                case Direction.Up:
+                    position.x -= 1;
+                    break;
+                case Direction.Down:
+                    position.x += 1;
+                    break;
+                case Direction.Left:
+                    position.y -= 1;
+                    break;
+                case Direction.Right:
+                    position.y += 1;
+                    break;
+                default:
+                    position.x += 0;
+                    position.y += 0;
+                    break;
+                  
+            }
+        }
+    }
+}
